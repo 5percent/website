2214558728c47165f14922833dcd049bb9800868
LU ZHE <zlu@li1066-174.members.linode.com>
2015-11-15 19:12:16 +0000

    update

---
+++ routers.js
@@ -0,0 +1,9 @@
+exports.home = function (req, res) {
+    res.sendfile(__dirname + '/views/home.html');
+}
+exports.snake = function (req, res) {
+    res.sendfile(__dirname + '/views/snake.html');
+}
+exports.tetris = function (req, res) {
+    res.sendfile(__dirname + '/views/tetris.html');
+}
---
+++ server.js
@@ -0,0 +1,14 @@
+// system
+var express = require('express');
+var app = express();
+
+// custom
+var routers = require('./routers');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routers.home);
+app.get('/games/snake', routers.snake);
+app.get('/games/tetris', routers.tetris);
+
+var server = app.listen(80);
